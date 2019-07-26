442be344a900f829b55cc3903804b855f8eaf1ac
mehmetbozyel <mmtbozyel@gmail.com>
2019-07-26 11:17:53 +0300

    project created

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{justifyContent:'center', alignItems:'center', flex: 1}}>
+      <Text style={{fontSize:40}}>!!! BİR FİKRİM VAR !!!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
